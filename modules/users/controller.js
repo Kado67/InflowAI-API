538c0ec71f0d09835f29a56bbf0a454cfb9e54aa
Kado67 <125838921+Kado67@users.noreply.github.com>
2025-11-30 18:07:51 +0300

    Update controller.js

--- modules/users/controller.js
+++ modules/users/controller.js
@@ -1,1 +1,112 @@
+// modules/users/controller.js
 
+const userService = require('./service');
+
+async function getProfile(req, res) {
+  try {
+    const profile = await userService.getProfile(req.user.id);
+
+    return res.json({
+      success: true,
+      data: profile,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+}
+
+async function updateProfile(req, res) {
+  try {
+    const updated = await userService.updateProfile(req.user.id, req.body);
+
+    return res.json({
+      success: true,
+      data: updated,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+}
+
+async function addAddress(req, res) {
+  try {
+    const list = await userService.addAddress(req.user.id, req.body);
+
+    return res.json({
+      success: true,
+      data: list,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+}
+
+async function updateAddress(req, res) {
+  try {
+    const updated = await userService.updateAddress(
+      req.user.id,
+      req.params.addressId,
+      req.body
+    );
+
+    return res.json({
+      success: true,
+      data: updated,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+}
+
+async function deleteAddress(req, res) {
+  try {
+    await userService.deleteAddress(req.user.id, req.params.addressId);
+
+    return res.json({
+      success: true,
+      message: "Adres silindi.",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+}
+
+async function listAddresses(req, res) {
+  try {
+    const list = await userService.listAddresses(req.user.id);
+
+    return res.json({
+      success: true,
+      data: list,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+}
+
+module.exports = {
+  getProfile,
+  updateProfile,
+  addAddress,
+  updateAddress,
+  deleteAddress,
+  listAddresses,
+};
