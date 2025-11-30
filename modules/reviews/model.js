dca2d1ccfe3525ef635f58ca66912eaf07d7a94f
Kado67 <125838921+Kado67@users.noreply.github.com>
2025-11-30 18:54:11 +0300

    Update model.js

--- modules/reviews/model.js
+++ modules/reviews/model.js
@@ -1,1 +1,36 @@
+// modules/reviews/model.js
 
+import mongoose from "mongoose";
+
+const ReviewSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+
+    comment: { type: String, default: "" },
+
+    isApproved: { type: Boolean, default: true },
+  },
+  { timestamps: true }
+);
+
+// bir kullanıcı aynı ürüne sadece 1 yorum yapabilir
+ReviewSchema.index({ user: 1, product: 1 }, { unique: true });
+
+export default mongoose.model("Review", ReviewSchema);
