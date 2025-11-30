4767fa8460813efd4cd8a0e1519bf9edbf067750
Kado67 <125838921+Kado67@users.noreply.github.com>
2025-11-30 18:49:21 +0300

    Update model.js

--- modules/payments/model.js
+++ modules/payments/model.js
@@ -1,1 +1,40 @@
+// modules/payments/model.js
 
+import mongoose from "mongoose";
+
+const PaymentSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    order: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
+      required: true,
+    },
+
+    amount: { type: Number, required: true },
+
+    method: {
+      type: String,
+      enum: ["paytr", "credit_card", "eft", "cod"],
+      default: "paytr",
+    },
+
+    status: {
+      type: String,
+      enum: ["pending", "paid", "failed"],
+      default: "pending",
+    },
+
+    transactionId: { type: String }, // PayTR Sipariş ID
+    message: { type: String },       // hata veya açıklama
+
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Payment", PaymentSchema);
