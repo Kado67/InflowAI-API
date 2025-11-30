e7a89c9ce21a2922730a45c05c6a66820ff3b239
Kado67 <125838921+Kado67@users.noreply.github.com>
2025-11-30 18:45:39 +0300

    Update service.js

--- modules/suppliers/service.js
+++ modules/suppliers/service.js
@@ -1,1 +1,25 @@
+// modules/suppliers/service.js
 
+import Supplier from "./model.js";
+
+export default {
+  async createSupplier(data) {
+    return await Supplier.create(data);
+  },
+
+  async getSuppliers() {
+    return await Supplier.find().sort({ createdAt: -1 });
+  },
+
+  async getSupplier(id) {
+    return await Supplier.findById(id);
+  },
+
+  async updateSupplier(id, data) {
+    return await Supplier.findByIdAndUpdate(id, data, { new: true });
+  },
+
+  async deleteSupplier(id) {
+    return await Supplier.findByIdAndDelete(id);
+  },
+};
