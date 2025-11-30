c0bd947cb98bfe54b0e4622a058ea11d73cd3213
Kado67 <125838921+Kado67@users.noreply.github.com>
2025-11-30 18:24:17 +0300

    Update model.js

--- modules/orders/model.js
+++ modules/orders/model.js
@@ -1,1 +1,52 @@
+// modules/orders/model.js
 
+import mongoose from "mongoose";
+
+const OrderItemSchema = new mongoose.Schema(
+  {
+    product: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+    name: String,
+    price: Number,
+    quantity: Number,
+    image: String,
+  },
+  { _id: false }
+);
+
+const OrderSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+
+    items: [OrderItemSchema],
+
+    totalAmount: { type: Number, required: true },
+    shippingAddress: {
+      fullName: String,
+      phone: String,
+      city: String,
+      district: String,
+      addressLine: String,
+      postalCode: String,
+    },
+
+    paymentStatus: {
+      type: String,
+      enum: ["pending", "paid", "failed"],
+      default: "pending",
+    },
+
+    orderStatus: {
+      type: String,
+      enum: ["pending", "processing", "shipped", "delivered", "canceled"],
+      default: "pending",
+    },
+
+    trackingNumber: { type: String },
+    shippingCompany: { type: String },
+
+    isPaid: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Order", OrderSchema);
