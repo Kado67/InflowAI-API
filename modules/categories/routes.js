7c27d790a31dc5fb69be80cb84d0eb91b3139696
Kado67 <125838921+Kado67@users.noreply.github.com>
2025-11-30 18:20:57 +0300

    Update routes.js

--- modules/categories/routes.js
+++ modules/categories/routes.js
@@ -1,1 +1,12 @@
+import { Router } from "express";
+import Controller from "./controller.js";
 
+const router = Router();
+
+router.post("/", Controller.createCategory);
+router.get("/", Controller.getAllCategories);
+router.get("/:id", Controller.getCategory);
+router.put("/:id", Controller.updateCategory);
+router.delete("/:id", Controller.deleteCategory);
+
+export default router;
