fdd94a1287e1d61ee159ef75762e306a651d2f18
Kado67 <125838921+Kado67@users.noreply.github.com>
2025-11-30 18:39:50 +0300

    Update model.js

--- modules/wishlist/model.js
+++ modules/wishlist/model.js
@@ -1,1 +1,30 @@
+// modules/wishlist/model.js
 
+import mongoose from "mongoose";
+
+const WishlistSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    items: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        addedAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Wishlist", WishlistSchema);
