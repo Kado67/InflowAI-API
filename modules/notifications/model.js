5ef47ed22f5867faca5e6b0cb630733555276700
Kado67 <125838921+Kado67@users.noreply.github.com>
2025-11-30 18:29:52 +0300

    Update model.js

--- modules/notifications/model.js
+++ modules/notifications/model.js
@@ -1,1 +1,43 @@
+// modules/notifications/model.js
 
+import mongoose from "mongoose";
+
+const NotificationSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    title: {
+      type: String,
+      required: true,
+    },
+
+    message: {
+      type: String,
+      required: true,
+    },
+
+    type: {
+      type: String,
+      enum: [
+        "order",
+        "shipping",
+        "promotion",
+        "system",
+        "message",
+      ],
+      default: "system",
+    },
+
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Notification", NotificationSchema);
