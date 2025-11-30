febca84a6dac3e0c0133756362ac00366f2f2aa6
Kado67 <125838921+Kado67@users.noreply.github.com>
2025-11-30 18:07:16 +0300

    Update service.js

--- modules/users/service.js
+++ modules/users/service.js
@@ -1,1 +1,55 @@
+// modules/users/service.js
 
+const User = require('./model');
+
+async function getProfile(userId) {
+  return User.findById(userId).select("-password");
+}
+
+async function updateProfile(userId, data) {
+  return User.findByIdAndUpdate(userId, data, { new: true })
+    .select("-password");
+}
+
+async function addAddress(userId, addressData) {
+  const user = await User.findById(userId);
+  user.addresses.push(addressData);
+  await user.save();
+  return user.addresses;
+}
+
+async function updateAddress(userId, addressId, data) {
+  const user = await User.findById(userId);
+
+  const addr = user.addresses.id(addressId);
+  if (!addr) throw new Error("Adres bulunamadı.");
+
+  Object.assign(addr, data);
+  await user.save();
+  return addr;
+}
+
+async function deleteAddress(userId, addressId) {
+  const user = await User.findById(userId);
+
+  const addr = user.addresses.id(addressId);
+  if (!addr) throw new Error("Adres bulunamadı.");
+
+  addr.deleteOne();
+  await user.save();
+  return true;
+}
+
+async function listAddresses(userId) {
+  const user = await User.findById(userId);
+  return user.addresses;
+}
+
+module.exports = {
+  getProfile,
+  updateProfile,
+  addAddress,
+  updateAddress,
+  deleteAddress,
+  listAddresses,
+};
