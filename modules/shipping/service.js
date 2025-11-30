ec93b0525e5a49f0733f4c36bdec2c4fd78f0cce
Kado67 <125838921+Kado67@users.noreply.github.com>
2025-11-30 18:58:39 +0300

    Update service.js

--- modules/shipping/service.js
+++ modules/shipping/service.js
@@ -1,1 +1,39 @@
+// modules/shipping/service.js
 
+import ShippingCompany from "./model.js";
+import Order from "../orders/model.js";
+
+export default {
+  async createCompany(data) {
+    return await ShippingCompany.create(data);
+  },
+
+  async listCompanies() {
+    return await ShippingCompany.find().sort({ createdAt: -1 });
+  },
+
+  async getCompany(id) {
+    return await ShippingCompany.findById(id);
+  },
+
+  async updateCompany(id, data) {
+    return await ShippingCompany.findByIdAndUpdate(id, data, { new: true });
+  },
+
+  async deleteCompany(id) {
+    return await ShippingCompany.findByIdAndDelete(id);
+  },
+
+  async assignShippingToOrder(orderId, data) {
+    const order = await Order.findById(orderId);
+    if (!order) throw new Error("Sipariş bulunamadı");
+
+    order.orderStatus = "shipped";
+    order.shippingCompany = data.company;
+    order.trackingNumber = data.trackingNumber;
+
+    await order.save();
+
+    return order;
+  },
+};
