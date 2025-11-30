1d0ef051d983317d7ce22458ea3ad34724731062
Kado67 <125838921+Kado67@users.noreply.github.com>
2025-11-30 20:19:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
 // index.js
-// =========================================
-// InflowAI API - Başlatıcı
-// =========================================
-
-const app = require("./app");
-const PORT = process.env.PORT || 10000;
-
-app.listen(PORT, () => {
-  console.log("////////////////////////////////////////");
-  console.log(` InflowAI API ÇALIŞIYOR → PORT: ${PORT}`);
-  console.log(" Ortak motor online ✔");
-  console.log("////////////////////////////////////////");
-});
+import "./app.js";
