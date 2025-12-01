d97ed9f2043c43c4e5b96d7e2cb7efa5d229ce9c
Kado67 <125838921+Kado67@users.noreply.github.com>
2025-12-01 18:56:47 +0300

    Update index.js

--- modules/products/index.js
+++ modules/products/index.js
@@ -1,2 +1,16 @@
-// modules/products/index.js
-module.exports = require('./routes');
+import express from "express";
+import Product from "./model.js";
+
+const router = express.Router();
+
+// TÜM ÜRÜNLER
+router.get("/", async (req, res, next) => {
+  try {
+    const products = await Product.find();
+    res.json({ success: true, products });
+  } catch (err) {
+    next(err);
+  }
+});
+
+export default router;
