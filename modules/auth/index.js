5c89fe8850c680eaed717ae8110887b57b80b38b
Kado67 <125838921+Kado67@users.noreply.github.com>
2025-12-01 18:54:55 +0300

    Update index.js

--- modules/auth/index.js
+++ modules/auth/index.js
@@ -1,7 +1,54 @@
-// modules/auth/index.js
+import express from "express";
+import { login, register, logout, refreshTokens } from "./service.js";
 
-import routes from "./routes.js";
+const router = express.Router();
 
-export default {
-  routes,
-};
+// REGISTER
+router.post("/register", async (req, res, next) => {
+  try {
+    const data = await register(req.body);
+    res.json({ success: true, ...data });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// LOGIN
+router.post("/login", async (req, res, next) => {
+  try {
+    const data = await login({
+      ...req.body,
+      userAgent: req.headers["user-agent"],
+      ip: req.ip,
+    });
+    res.json({ success: true, ...data });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// LOGOUT
+router.post("/logout", async (req, res, next) => {
+  try {
+    await logout(req.body.userId, req.body.refreshToken);
+    res.json({ success: true });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// REFRESH TOKEN
+router.post("/refresh", async (req, res, next) => {
+  try {
+    const data = await refreshTokens(
+      req.body.refreshToken,
+      req.headers["user-agent"],
+      req.ip
+    );
+    res.json({ success: true, ...data });
+  } catch (err) {
+    next(err);
+  }
+});
+
+export default router;
