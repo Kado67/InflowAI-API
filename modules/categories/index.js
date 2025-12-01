c7914f6fc9d3daf5783e6b5ebead48311f3803ba
Kado67 <125838921+Kado67@users.noreply.github.com>
2025-12-01 18:57:31 +0300

    Update index.js

--- modules/categories/index.js
+++ modules/categories/index.js
@@ -1,2 +1,15 @@
-import routes from "./routes.js";
-export default routes;
+import express from "express";
+import Category from "./model.js";
+
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const categories = await Category.find();
+    res.json({ success: true, categories });
+  } catch (err) {
+    next(err);
+  }
+});
+
+export default router;
