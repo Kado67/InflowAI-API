cce72239b7c11fbbdcb75d97eefa555d9aa7d66f
Kado67 <125838921+Kado67@users.noreply.github.com>
2025-12-04 19:26:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-// index.js — InflowAI API server starter
+// index.js — InflowAI API (ES MODULE uyumlu)
 
-const app = require("./app");
-const http = require("http");
+import app from "./app.js";
+import http from "http";
+
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log(`InflowAI API çalışıyor → PORT: ${PORT}`);
+  console.log(`InflowAI API çalışıyor → PORT ${PORT}`);
 });
